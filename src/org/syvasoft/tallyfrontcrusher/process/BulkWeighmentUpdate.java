7c121fc1afe781941b4eed5518cf70f39144d532
Syed <syed@syvasoft.com>
2023-09-27 20:01:54 +0530

    Create BulkWeighmentUpdate.java

---
+++ src/org/syvasoft/tallyfrontcrusher/process/BulkWeighmentUpdate.java
@@ -0,0 +1,19 @@
+package org.syvasoft.tallyfrontcrusher.process;
+
+import org.compiere.process.SvrProcess;
+
+public class BulkWeighmentUpdate extends SvrProcess {
+
+	@Override
+	protected void prepare() {
+		
+
+	}
+
+	@Override
+	protected String doIt() throws Exception {
+		
+		return null;
+	}
+
+}
