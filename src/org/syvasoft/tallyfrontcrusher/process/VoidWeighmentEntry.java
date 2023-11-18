bb7cb31e34de3cf51b645a69f3e9cfb12b4e6ed6
balamuruugan <67673357+balamuruugan@users.noreply.github.com>
2023-11-18 14:07:04 +0530

    Update VoidWeighmentEntry.java

--- src/org/syvasoft/tallyfrontcrusher/process/VoidWeighmentEntry.java
+++ src/org/syvasoft/tallyfrontcrusher/process/VoidWeighmentEntry.java
@@ -38,50 +38,60 @@ public class VoidWeighmentEntry extends SvrProcess {
 
 	@Override
 	protected String doIt() throws Exception {
-		MWeighmentEntry wEntry = new MWeighmentEntry(getCtx(), TF_WeighmentEntry_ID, get_TrxName());
+		String whereClause = " Status IN ('CL','CO', 'ER','P', 'UR') AND (EXISTS (SELECT T_Selection_ID FROM T_Selection WHERE " +
+				" T_Selection.AD_PInstance_ID=? AND T_Selection.T_Selection_ID = TF_WeighmentEntry.TF_WeighmentEntry_ID) OR TF_WeighmentEntry_ID = ?) "
+				+ "  ";
+				//+ "AND C_Order.DocStatus IN ('CO','DR','IR'))";
+		int i = 0;
+		String oWhereClause = "TF_WeighmentEntry_ID = ? AND C_BPartner_ID = ? AND IsSOTrx = 'Y' AND DocStatus IN ('CO','CL')";
+		List<MWeighmentEntry> wEntries = new Query(getCtx(), MWeighmentEntry.Table_Name, whereClause, get_TrxName())
+				.setClient_ID()
+				.setParameters(getAD_PInstance_ID(), getRecord_ID())
+				.list();
 		
-		
-		if(wEntry.getDescription() != null && wEntry.getDescription().contains("ERROR:") && wEntry.getStatus().equals(MWeighmentEntry.STATUS_Error)) 
-			wEntry.setDescription(null);
-		
-		Trx trx = Trx.get(get_TrxName(), false);
-		Savepoint sp = null;
-		try {
-			String msg = null;
-			sp = trx.setSavepoint(wEntry.getDocumentNo());
+		for(MWeighmentEntry wEntry : wEntries) {
 			
-			if(wEntry.getWeighmentEntryType().equalsIgnoreCase(MWeighmentEntry.WEIGHMENTENTRYTYPE_Sales) || 
-					wEntry.getWeighmentEntryType().equalsIgnoreCase(MWeighmentEntry.WEIGHMENTENTRYTYPE_Input)) {
-				if(wEntry.getStatus().equals(MWeighmentEntry.STATUS_Billed) && !wEntry.isRequiredTaxInvoicePerLoad()) {
-					throw new AdempiereException("Weighment Entry No: " + wEntry.getDocumentNo() +" cannot be modified since it is billed under Consolidated Invoice.!");
-				}
-			}
-			if(wEntry.getWeighmentEntryType().equalsIgnoreCase(MWeighmentEntry.WEIGHMENTENTRYTYPE_SubcontractProductionReceipt) || 
-					wEntry.getWeighmentEntryType().equalsIgnoreCase(MWeighmentEntry.WEIGHMENTENTRYTYPE_StockToCrusher)) {
-				String whereClause = " M_Product_ID = ? AND M_Warehouse_ID = ?  AND TRUNC(DateAcct) = ? AND DocStatus IN ('CO','CL') ";
+			if(wEntry.getDescription() != null && wEntry.getDescription().contains("ERROR:") && wEntry.getStatus().equals(MWeighmentEntry.STATUS_Error)) 
+				wEntry.setDescription(null);
+			
+			Trx trx = Trx.get(get_TrxName(), false);
+			Savepoint sp = null;
+			try {
+				String msg = null;
+				sp = trx.setSavepoint(wEntry.getDocumentNo());
 				
-				List<MBoulderWastageHdr> bhdr = new Query(getCtx(), MBoulderWastageHdr.Table_Name, whereClause, get_TrxName())
-											.setClient_ID()
-											.setParameters(wEntry.getM_Product_ID(), wEntry.getM_Warehouse_ID(), wEntry.getDateAcct())
-											.list();					
-				if(bhdr.size() > 0) {
-					MMessage message = MMessage.get(getCtx(), "ModifyWE_BoulderWastage");
-					throw new AdempiereException(message.getMsgText());
+				if(wEntry.getWeighmentEntryType().equalsIgnoreCase(MWeighmentEntry.WEIGHMENTENTRYTYPE_Sales) || 
+						wEntry.getWeighmentEntryType().equalsIgnoreCase(MWeighmentEntry.WEIGHMENTENTRYTYPE_Input)) {
+					if(wEntry.getStatus().equals(MWeighmentEntry.STATUS_Billed) && !wEntry.isRequiredTaxInvoicePerLoad()) {
+						throw new AdempiereException("Weighment Entry No: " + wEntry.getDocumentNo() +" cannot be modified since it is billed under Consolidated Invoice.!");
+					}
 				}
+				if(wEntry.getWeighmentEntryType().equalsIgnoreCase(MWeighmentEntry.WEIGHMENTENTRYTYPE_SubcontractProductionReceipt) || 
+						wEntry.getWeighmentEntryType().equalsIgnoreCase(MWeighmentEntry.WEIGHMENTENTRYTYPE_StockToCrusher)) {
+					whereClause = " M_Product_ID = ? AND M_Warehouse_ID = ?  AND TRUNC(DateAcct) = ? AND DocStatus IN ('CO','CL') ";
+					
+					List<MBoulderWastageHdr> bhdr = new Query(getCtx(), MBoulderWastageHdr.Table_Name, whereClause, get_TrxName())
+												.setClient_ID()
+												.setParameters(wEntry.getM_Product_ID(), wEntry.getM_Warehouse_ID(), wEntry.getDateAcct())
+												.list();					
+					if(bhdr.size() > 0) {
+						MMessage message = MMessage.get(getCtx(), "ModifyWE_BoulderWastage");
+						throw new AdempiereException(message.getMsgText());
+					}
+				}
+				wEntry.setStatus(MWeighmentEntry.STATUS_Voided);
+				wEntry.setProcessed(false);
+				wEntry.saveEx();
+				
+				trx.releaseSavepoint(sp);
 			}
-			wEntry.setStatus(MWeighmentEntry.STATUS_Voided);
-			wEntry.setProcessed(false);
-			wEntry.saveEx();
+			catch (Exception ex) {
+				if(sp != null)
+					trx.rollback(sp);
 			
-			trx.releaseSavepoint(sp);
+				throw ex;
+			}			
 		}
-		catch (Exception ex) {
-			if(sp != null)
-				trx.rollback(sp);
-		
-			throw ex;
-		}			
-
 		return null;
 	}
 
