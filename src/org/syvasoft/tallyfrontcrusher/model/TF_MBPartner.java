7e3a233a2bc1b87a613ace5e7ad0e96e407a5c88
MohamedFharuk <120706329+MohamedFharuk@users.noreply.github.com>
2024-01-29 18:53:25 +0530

    Revert "Update TF_MBPartner.java"
    This reverts commit d5b7637155dc695233883b7adedcb4c6ca2735df.

--- src/org/syvasoft/tallyfrontcrusher/model/TF_MBPartner.java
+++ src/org/syvasoft/tallyfrontcrusher/model/TF_MBPartner.java
@@ -1183,24 +1183,26 @@ public class TF_MBPartner extends MBPartner {
 		if(IsRequiredTaxInvoicePerLoad()) {
 			setTF_TaxInvoiceCycle_ID(0);
 		}
-		String Whereclause = " TaxID = '"+getTaxID()+"'";
+		String Whereclause = " iscustomer = 'Y' and TaxID = '"+getTaxID()+"'";
 		TF_MBPartner bpcustomer = new Query(getCtx(), TF_MBPartner.Table_Name, Whereclause, get_TrxName()).first();
-		
+		String Whereclause1 = " isvendor = 'Y' and TaxID = '"+getTaxID()+"'";
+		TF_MBPartner bpvendor = new Query(getCtx(), TF_MBPartner.Table_Name, Whereclause1, get_TrxName()).first();
 		if(bpcustomer!=null&&isCustomer()) {
 			throw new AdempiereException("GST No Already Exists");
 		}
-		if(bpcustomer != null&&isVendor()) {
+		if(bpvendor != null&&isVendor()) {
 			throw new AdempiereException("GST No Already Exists");
 		}
 		String panno = (String) get_Value("PanNo");
 		
-		String Whereclause2 = " PanNo = '"+panno+"'";
+		String Whereclause2 = " iscustomer = 'Y' and PanNo = '"+panno+"'";
 		TF_MBPartner pancustomer = new Query(getCtx(), TF_MBPartner.Table_Name, Whereclause2, get_TrxName()).first();
-		
+		String Whereclause3 = " isvendor = 'Y' and PanNo = '"+panno+"'";
+		TF_MBPartner panvendor = new Query(getCtx(), TF_MBPartner.Table_Name, Whereclause3, get_TrxName()).first();
 		if(pancustomer!=null&&isCustomer()) {
 			throw new AdempiereException("Pan No Already Exists");
 		}
-		if(pancustomer!=null&&isVendor()) {
+		if(panvendor!=null&&isVendor()) {
 			throw new AdempiereException("Pan No Already Exists");
 		}
 		return super.beforeSave(newRecord);
